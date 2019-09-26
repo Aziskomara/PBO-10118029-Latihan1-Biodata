d1ce2a67c8ba4ff5a471cbbe6a65d9b6cbf405c6
Aziskomara <55784203+Aziskomara@users.noreply.github.com>
2019-09-26 15:15:03 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg1;
+
+/**
+ * Nama     : Azis komara
+ * Kelas    : IF-1
+ * Nim      : 10118029
+ * deskiripsi program   : menampilkan Biodata
+ * 
+ * @author Azis komara
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-------------------");
+        System.out.println("NAMA\t\t: AZIS KOMARA");
+        System.out.println("ALAMAT\t\t: Kp.Pasirjadi");
+        System.out.println("Tempat Lahir\t: Subang");
+        System.out.println("Tanggal Lahir\t: 16 April 2000");
+        System.out.println("-----------------");
+        System.out.println("HOBY\t\t: Volly");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Jus Mangga");
+    }
+    
+}
